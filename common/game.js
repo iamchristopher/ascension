1d2ec24f8beda229aee1bba91e6d5b7ee1c32f7e
iamchristopher <chriswright@live.com>
2018-07-05 21:39:58 -0400

    Fixed attacks aren't cancelled correctly

--- common/game.js
+++ common/game.js
@@ -185,30 +185,35 @@ export default {
                 name: 'Attack',
                 allowedMoves: [ 'attackPawn' ],
                 onPhaseBegin (G, ctx) {
-                    return modifyPlayer(G, ctx.currentPlayer, {
+                    const pawnId = findActivePawn(G.pawns);
+
+                    return modifyPawn(G, pawnId, {
                         actions: 1
                     });
                 },
                 onMove (G, ctx) {
-                    const currentPlayer = G.players[ctx.currentPlayer];
+                    const pawnId = findActivePawn(G.pawns);
+                    const pawn = G.pawns[pawnId];
 
-                    return modifyPlayer(G, ctx.currentPlayer, {
-                        actions: currentPlayer.actions - 1
+                    return modifyPawn(G, pawnId, {
+                        actions: pawn.actions - 1
                     });
                 },
                 endPhaseIf (G, ctx) {
-                    const currentPlayer = G.players[ctx.currentPlayer];
+                    const pawnId = findActivePawn(G.pawns);
+                    const pawn = G.pawns[pawnId];
 
-                    return currentPlayer.actions <= 0 && 'Activation';
+                    return pawn.actions <= 0 && 'Activation';
                 },
                 onPhaseEnd (G, ctx) {
-                    const currentPlayer = G.players[ctx.currentPlayer];
+                    const pawnId = findActivePawn(G.pawns);
+                    const pawn = G.pawns[pawnId];
 
-                    return modifyPlayer(G, ctx.currentPlayer, {
+                    return modifyPawn(G, pawnId, {
                         actions: 0,
-                        activations: currentPlayer.actions === 1
-                            ?   currentPlayer.activations - 1
-                            :   currentPlayer.activations
+                        activations: pawn.actions === 1
+                            ?   pawn.activations - 1
+                            :   pawn.activations
                     });
                 }
             }
