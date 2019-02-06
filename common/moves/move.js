ddba0f56151fc1cf3bafcfdc1d82ecf5f17e7005
iamchristopher <chriswright@live.com>
2019-02-05 21:22:57 -0500

    Changed movement phase is now ended if tagged tile is encountered

--- common/moves/move.js
+++ common/moves/move.js
@@ -3,6 +3,26 @@ import {
 } from '../util';
 
 export default function (G, ctx, id, position) {
+    const tileX = Math.floor(position.x / 50);
+    const tileY = Math.floor(position.y / 50);
+    const roomId = G.map.rooms.findIndex(({ height, width, x, y, }) => (
+        x <= tileX &&
+        y <= tileY &&
+        x + width >= tileX &&
+        y + height >= tileY
+    ));
+    const room = G.map.rooms[roomId];
+    const tile = room.tiles[tileY - room.y][tileX - room.x];
+
+    if (tile.tags.length > 0) {
+        ctx.events.endPhase();
+
+        return modifyPawn(G, id, {
+            position,
+            triggered: true, // TODO: Do something with this
+        });
+    }
+
     return modifyPawn(G, id, {
         position
     });
