ba8431b5096c006f33ec2f3ccd2be6b12ac06c28
jorbascrumps <chriswright@live.com>
2019-03-09 22:27:14 -0500

    Updated attack move to use active pawn

--- common/moves/attack.js
+++ common/moves/attack.js
@@ -1,15 +1,18 @@
 import {
+    findActivePawn,
     modifyPawn
 } from '../util';
 
-export default function (G, ctx, attackerID, defenderID) {
-    const defender = G.pawns[defenderID];
+export default function (G, ctx, target) {
+    const pawnId = findActivePawn(G.pawns);
+    console.log(pawnId, target);
+
+    const defender = G.pawns[target];
     const newHealth = defender.currentHealth - 10 <= 0
         ?   0
         :   defender.currentHealth - 10;
 
-
-    return modifyPawn(G, defenderID, {
-        currentHealth: newHealth
+    return modifyPawn(G, target, {
+        currentHealth: newHealth,
     });
 }
