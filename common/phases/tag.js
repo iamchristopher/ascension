2e53f29d7f3917e3f753d935083f981c647a0b42
jorbascrumps <chriswright@live.com>
2019-03-09 22:25:45 -0500

    Updated tag phase to correctly track player actions

--- common/phases/tag.js
+++ common/phases/tag.js
@@ -1,46 +1,36 @@
 import * as MOVES from '../constants/moves';
 import * as PHASES from '../constants/phases';
 import {
-    findActivePawn,
-    modifyPawn
+    modifyPlayer,
 } from '../util';
 
 export default {
     allowedMoves: [ MOVES.TAG ],
     onPhaseBegin (G, ctx) {
-        const pawnId = findActivePawn(G.pawns);
-
-        return modifyPawn(G, pawnId, {
-            actions: 1
+        return modifyPlayer(G, ctx.currentPlayer, {
+            actions: 1,
         });
     },
     onMove (G, ctx) {
-        const pawnId = findActivePawn(G.pawns);
-        const pawn = G.pawns[pawnId];
-
-        return modifyPawn(G, pawnId, {
-            actions: pawn.actions - 1
+        return modifyPlayer(G, ctx.currentPlayer, {
+            actions: 0,
         });
     },
     endPhaseIf (G, ctx) {
-        const pawnId = findActivePawn(G.pawns);
-        const pawn = G.pawns[pawnId];
+        const {
+            players: {
+                [ctx.currentPlayer]: {
+                    actions,
+                },
+            },
+        } = G;
 
-        if (pawn.actions <= 0) {
-            return {
-                next: PHASES.ACTIVATION,
-            };
+        if (actions) {
+            return;
         }
-    },
-    onPhaseEnd (G, ctx) {
-        const pawnId = findActivePawn(G.pawns);
-        const pawn = G.pawns[pawnId];
 
-        return modifyPawn(G, pawnId, {
-            actions: 0,
-            activations: pawn.actions === 1
-                ?   pawn.activations - 1
-                :   pawn.activations
-        });
+        return {
+            next: PHASES.OVERLORD,
+        };
     },
 };
